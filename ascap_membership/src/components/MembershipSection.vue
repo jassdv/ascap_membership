a223fe59e5b6bf2764337b6ac1332a3f27a4f968
jasmine.zangi <jasmine.zangi@gmail.com>
2020-01-24 17:20:33 -0500

    section and button components

---
+++ ascap_membership/src/components/MembershipSection.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <header>
+        
+    </header>
+    <div>
+        <slot />
+        <div> </div>
+    </div>
+</div>
+
+    
+</template>
+<script>
+export default {
+  props: {
+    headerTitle: {
+      type: String,
+      required: true
+    },
+    sectionNumber: {
+        type: Number,
+        required: true
+    }
+  },
+    
+}
+</script>
+<style scoped>
+
+</style>
---
+++ ascap_membership/src/components/SectionButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <button v-bind:class="style"  @click="click">{{label}}</button>
+    
+</template>
+<script>
+export default {
+  props: {
+    click: {
+      type: Function,
+      required: false
+    },
+    label: {
+        type: String,
+        required: true
+    }
+  },
+  computed: {
+      //check if that could be a data object instead of computed
+      style () {
+          return this.label == 'continue' ? 'btn mb-2 uppercase' : 'btn mb-2 cancel uppercase';
+      }
+    },
+    methods: {
+        
+    }
+
+    
+}
+</script>
+<style scoped>
+.uppercase{
+    text-transform: uppercase;
+}
+.cancel{
+    background-color: #fff;
+    color: #0d0d0d;
+    border: 1px solid #ededed;
+}
+
+
+</style>
